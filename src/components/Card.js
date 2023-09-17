6b532038e3dfbaba307db873683058b23eccfb24
JeanneGbn <j.goubin@gmail.com>
2023-09-17 18:47:50 +0200

    Error console

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,14 +6,16 @@ function Card() {
     return (
         <div className='card'>
             {annonces.map((annonces) => (
+                <Link to={`/D_Fiche-Logement/${annonces.id}`}>
                 <div className='card__div' key={annonces.id}>
-                    <Link to={`/D_Fiche-Logement/${annonces.id}`}>
+                    
                         <img src={annonces.cover} alt={annonces.title}></img>
                         <div className='card__overlay'>
                         <p className='card__title'>{annonces.title}</p>
                         </div>
-                    </Link>
+                    
                 </div>
+                </Link>
 
             ))}
         </div>
